35be9672c7eb848c58afccfa7a716752a56295d7
Sudarshan1216 <155080222+Sudarshan1216@users.noreply.github.com>
2024-06-09 22:54:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge;chrome=1" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="google" content="notranslate" />
+        <!-- Metadata (autofilled by "Save to HTML") -->
+        <meta name="author" content="sudarshan" />
+        <meta name="subject" content="A really good software engineer you should hire" />
+        <meta name="keywords" content="coding, developing, hacking" />
+        <meta name="date" content="2009-04-01" />
+        <meta name="generator" content="html-resume-template" />
+        <!-- Google Fonts, Normalize, and Font Awesome -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Cardo|Montserrat:300,400,500&amp;subset=latin-ext" crossorigin="anonymous" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" integrity="sha256-oSrCnRYXvHG31SBifqP2PM1uje7SJUyX0nTwO2RJV54=" crossorigin="anonymous" />
+        <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Custom Styles -->
+        <link rel="stylesheet" type="text/css" href="./paper.css" />
+        <link rel="stylesheet" type="text/css" href="./styles.css" />
+        <link rel="stylesheet" type="text/css" href="./typography.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="./screen.css" />
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <title>My Resume</title>
+    <style>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 15px;
+    background-color: gray;
+}
+
+sud {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+h1 {
+    text-align: center;
+    color: #333;
+}
+
+table {
+    width: 98%;
+    border-collapse: collapse;
+    margin-top: 8px;
+}
+
+thead {
+    color: #fff;
+}
+
+th, td {
+    padding: 8px 10px;
+    text-align: left;
+    border: 1px solid #ddd;
+}
+
+tbody tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+tbody tr:hover {
+    background-color: #e9e9e9;
+}
+
+th {
+    color: black;
+}
+
+
+.contact, .skills, .technologies, .address {
+    width: 110%;
+    padding: 10px;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size:13px;
+}
+
+.contact ul, .skills ul, .technologies ul, .address ul {
+    list-style-type: none;
+    padding: 0;
+    font-size:13px;
+}
+
+.contact h4, .skills h4, .technologies h4, .address h4 {
+    margin-bottom: 6px;
+    font-size:13px;
+}
+
+address {
+    font-family: Arial, sans-serif;
+    line-height: 1;
+    margin-bottom:10px;
+}
+header.name {
+    margin-bottom: 20px; 
+    margin-left: 20px; 
+    height:30px;
+}
+
+.education {
+    margin-left: 25px; 
+    height: auto;
+    margin-bottom:10px;
+}
+.projects{
+    margin-left:25px;
+    margin-top: 10;
+}
+.img {
+    float: right; 
+    margin-left: 20px;
+    margin-top: 5px; 
+    width: 100px; 
+    height: auto; /* Maintain aspect ratio */
+}
+.sheet {
+    min-height: 40vh; /* Set a minimum height of 100% of the viewport height */
+}
+
+@media print {
+    body{
+        size: 7in 9.25in;
+        margin: 27mm 16mm 27mm 16mm;
+    }
+}
+    </style>
+
+</head>
+<body class="letter">
+    <section id="save">
+    <h2 style="text-align: center; font-size:300%;"><b>Resume</b></h2>
+    <section class="sheet">
+    <aside>
+        <section class="contact">
+            <h4 style="letter-spacing: 1px;"><b>Contact</b></h4>
+            <ul>
+                <li>
+                    <p><i class="fa fa-map-marker" title="Location"></i> <a href="https://maps.app.goo.gl/27CE1HfCWT7K1b1i6">Pencil Chowk , Baramati ,Pin- 413133</a></p>
+                </li>
+                <li>
+                    <p><i class="fa fa-phone" title="Call Phone"></i> <a href="tel: 7038676708">7038676708</a></p>
+                </li>
+                <li>
+                    <p><i class="fa fa-envelope" title="Email"></i> <a href="mailto: sudarshan12shinde@gmail.com">sudarshan12shinde@gmail.com</a></p>
+                </li>
+                <li>
+                    <p><i class="fa fa-github" title="Git hub"></i> <a href="https://github.com/Sudarshan1216">Git Hub</a></p>
+                </li>
+            </ul>
+        </section>
+        <section class="skills">
+            <h4 style="letter-spacing: 1px;"><b>Skills</b></h4>
+            <ul>
+                <li><span>HTML</span></li>
+                <li><span>CSS</span></li>
+                <li><span>Responsive Design</span></li>
+                <li><span>Front-End</span></li>
+                <li><span>Firebase</span></li>
+                <li><span>Git</span></li>
+                <li><span>Communication ,Teamwork <br>and Time Management</span></li>
+            </ul>    
+        </section>
+        <section class="skills">
+            <h4 style="letter-spacing: 1px;"><b>Technologies</b></h4>
+            <ul>
+                <li><span>HTML</span></li>
+                <li><span>CSS</span></li>
+                <li><span>Android Studio</span></li>
+                <li><span>Firebase</span></li>
+            </ul>
+        </section>
+        <section class="skills">
+            <h4 style="letter-spacing: 1px;"><b>Certificate</b></h4>
+            <ul>
+                <li><span>Front End Development – HTML by STP computer education.</span></li>
+                <li><span> Development Android App. </span></li>
+                <li><span> Mobile UI and UX Design for beginners </span></li>
+                <li><span>Firebase cloud messaging </span></li>
+            </ul>
+        </section>
+        <section class="skills">
+            <h4 style="letter-spacing: 1px;"><b>Hobbies</b></h4>
+            <ul>
+                <li><span>Playing Volleyball</span></li>
+                <li><span>Playing Cricket</span></li>
+            </ul>
+
+        </section>
+        <section class="address">
+            <h4 style="letter-spacing: 1px;"><b> Address</b></h4>
+            <ul>    
+                <address>
+                At Post Pokharapur<br>
+                Tal-Mohol ,Dist-Solapur,<br>
+                Maharashtra , India <br>
+                Pin-413248
+                </address>
+                
+        </section>
+    </aside>
+    <section>
+    <header class="name" aria-label="sudarshan shinde">
+                        <a href="https://sudarshan.site">
+                            <svg width="257px" height="35px" viewBox="0 0 257 35" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-family="Montserrat-Regular, Montserrat" font-size="48" font-weight="normal">
+                                    <g id="Letter" transform="translate(-54.000000, -140.000000)" fill="#484848">
+                                        <text id="Sudarshan Shinde">
+                                            <tspan class="red-text" x="54.728" y="174" font-size="28">Sudarshan Shinde <img class="img" src="shinde.jpg" alt="" ></tspan>
+                                        </text>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a><br>
+                        <h7>Web Developer And Android Developer</h7>
+                        <hr />
+                    </header>
+                    <div class="education">
+                        <h4>Educational Qualification</h4>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Qualification</th>
+                                <th>Passing Year</th>
+                                <th>University / Board </th>
+                                <th>Percentages (%)</th>
+                            </tr>
+                            </thead>                         
+                            <tbody>
+                                <tr>
+                                    <td>SSC</td>
+                                    <td>2017</td>
+                                    <td>Maharashtra state board</td>
+                                    <td>72.00% </td>
+                                </tr>
+                                <tr>
+                                    <td>HSC</td>
+                                    <td>2019</td>
+                                    <td>Maharashtra state board</td>
+                                    <td>64.00%</td>
+                                </tr>
+                                <tr>
+                                    <td>BCA</td>
+                                    <td>2022</td>
+                                    <td>SPPU University</td>
+                                    <td>73.37%</td>
+                                </tr>
+                                <tr>
+                                    <td>MCA</td>
+                                    <td>2024</td>
+                                    <td>SPPU University</td>
+                                    <td>Pursuing</td>
+                                </tr>
+                            </tbody>                            
+                        </table>
+                    </div>
+                    <div class="projects">
+                        <h4>Project Title</h4>
+                            <ul>
+                                <li>
+                                  <p style="line-height: 2;"> <b> 1) Building Management</b></p> 
+                                </li>
+                            </ul>
+                                <p>The Building Maintenance project is designed to streamline the management of residential buildings. It provides a comprehensive platform for administrators to manage various aspects such as user management, maintenance tracking, billing, and community management, while also offering users the ability to interact with these services.</p>
+                                <ul>
+                                    <li>
+                                        <p style="line-height: 2;"><b> A) Technologies Used</b></p>
+                                        <p>I) HTML</p>
+                                        <p>II) CSS</p>
+                                        <p>III) JavaScript</p>
+                                        <p>IV) php</p>
+                                        <p>V) MySql</p>
+                                    </li>
+                                </ul>
+                    </div>
+                    <div class="projects">
+                        <h4 >Android Project</h4>
+                        <ul>
+                            <li>
+                                <p style="line-height: 2;"><b>2) Calculator</b></p>
+                            </li>
+                        </ul>
+                        <p>The Calculator App is a simple yet versatile Android application developed using Android Studio. It provides basic arithmetic functions such as addition, subtraction, multiplication, and division. The app features a user-friendly interface with large, easy-to-read buttons and a responsive layout that adapts to various screen sizes. </p>
+                        <ul>
+                            <li>
+                                <p style="line-height: 2;"><b>B) Technologies Used</b></p>
+                                <p>I) Android Studio</p>
+                                <p>II) Java Programming Language</p>
+                                <p>III) XML for Layout Design</p>
+                                <p>IV) Firebase</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="projects">
+                        <h4 style="line-height: 1;">Personal Details</h4>
+                        <ul>
+                            <li><span>1) Name:- Mr.Sudarshan Jaypal Shinde</span></li>
+                            <li><span>2) Date of birth: 12-06-2001 </span></li>
+                            <li><span>3) Gender: Male </span></li>
+                            <li><span>4) Languages: Marathi, English, Hindi</span></li>
+                        </ul>
+                    </div>
+    </section>
+    </section>
+    </section>
+    <button id="saveHTMLButton" style="position: fixed; top: 20px; right: 20px;">Save HTML</button> 
+    <button id="printButton" style="position: fixed; top: 60px; right: 20px; width:98px;" >Print</button>
+    <script>
+        // Function to save the current HTML page
+        function saveHTML() {
+            var htmlContent = document.documentElement.outerHTML;
+            var blob = new Blob([htmlContent], {type: "text/html"});
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = 'resume.html';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+
+        // Function to print the current page
+        function printPage() {
+            window.print();
+        }
+
+        // Event listeners for buttons
+        document.getElementById("saveHTMLButton").addEventListener("click", saveHTML);
+        document.getElementById("printButton").addEventListener("click", printPage);
+    </script>
+</body>
+</html>
